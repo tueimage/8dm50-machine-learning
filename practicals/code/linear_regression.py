8f699e372bde700e1bd6223a6c8ba4728b0d53df
Ivana144 <54931435+Ivana144@users.noreply.github.com>
2020-09-09 15:56:00 +0200

    PDF function

--- practicals/code/linear_regression.py
+++ practicals/code/linear_regression.py
@@ -18,12 +18,22 @@ def lsq(X, y):
     return beta
 
 def calcMSE(X, y_true, beta):
-    y_pred = np.zeros([300,1])
+    y_pred = np.zeros(np.shape(y_true))
     for patient in range(len(y_true)):
         y_pred[patient] = beta[0,0]
         for feature in range(len(X[patient,:])):
             y_pred[patient] = y_pred[patient] + beta[feature+1] * X[patient, feature]
 
     # Mean Squared Error 
+    MSE = np.square(np.subtract(y_true,y_pred)).mean() 
+    return MSE
+
+def calcMSEmatrix(X, y_true, beta):
+    # add column of ones to X
+    ones = np.ones((len(X), 1))
+    X = np.concatenate((ones, X), axis=1)
+ 
+    y_pred = np.dot(X,beta)
+ 
     MSE = np.square(np.subtract(y_true,y_pred)).mean() 
     return MSE
