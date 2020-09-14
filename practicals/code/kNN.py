7c79deff58ab32742c5ba0e3cd8a4e9b6cb88f5f
Dirk Loeffen <d.w.m.loeffen@student.tue.nl>
2020-09-14 17:03:27 +0200

    Added numpy implementation to kNN.py

--- practicals/code/kNN.py
+++ practicals/code/kNN.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp
 
 def euclidean_distance(a, b):
     """calculate euclidean distance between two points a and b"""
@@ -52,3 +53,62 @@ def kNN_regression(X_train, y_train, X_test, k):
         index, dist = get_neighbours(X_train, x_test, k)
         y_pred[i] = np.sum(y_train[index,0])/k
     return y_pred[:, np.newaxis]
+
+def get_neighbour_targets_numpy(X_train, X_test, y_train, k):
+    """
+    Numpy implementation of k neirest neighbors
+
+    Parameters
+    ----------
+    X_train : numpy.ndarray
+        numpy array containing training samples
+    X_test : numpy.ndarray
+        numpy array containing test samples
+    y_train : numpy.ndarray
+        numpy array containing training targets
+    k : int
+        number of neighbors to account for (preferably odd)
+    regression : boolean
+        when False this function classifies, when True this function regresses
+
+    Returns
+    -------
+    y_predic : numpy.ndarray
+        numpy array containing predicted targets
+
+    """
+    # Create 3D matrix where the second dimension is the number of 
+    # samples in X and the third dimension is the number of features
+    # this has to be like this because now the last two axes of X_three
+    # and X are the same and can be subtracted easily
+    X_three = np.repeat(X_train[:, :, None], len(X_test), axis=2)
+    X_three = np.swapaxes(X_three, 1, 2)
+    
+    # Subtract features of samples from 3D matrix
+    # I'm not entirely sure this does what I think it does
+    X_subtract = X_three - X_test
+    
+    # Square, sum, square root, to get L2 norm
+    X_l2 = np.sqrt(np.sum(np.multiply(X_subtract, X_subtract), axis = 2))
+    
+    # Get indices of the smallest
+    # mind that argpartition does not sort, instead use np.argsort if wanted
+    X_idx_k = np.argpartition(X_l2, k, axis=0)[:k, :]
+    target_size = X_idx_k.shape
+    
+    # Get values and reshape
+    y_k = y_train[X_idx_k.flatten()]
+    y_k = y_k.reshape(target_size)
+    
+    return y_k
+
+
+def kNN_classification_numpy(X_train, X_test, y_train, k):
+    y_k = get_neighbour_targets_numpy(X_train, X_test, y_train, k)
+    
+    return np.swapaxes(sp.stats.mode(y_k)[0], 0, 1)
+
+def kNN_regression_numpy(X_train, X_test, y_train, k):
+    y_k = get_neighbour_targets_numpy(X_train, X_test, y_train, k)
+    
+    return np.mean(y_k, axis=0)[:, None]
