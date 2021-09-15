0d1c59327e57fef47a247ee1f6ff02b4807bc42c
SimoneRuiter <simoneruiter98@hotmail.com>
2021-09-15 11:10:48 +0200

    (weighted) linear regression
    completed linear regression model and function for weighted linear regression model

--- practicals/code/linear_regression.py
+++ practicals/code/linear_regression.py
@@ -16,3 +16,21 @@ def lsq(X, y):
     beta = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
 
     return beta
+
+def mse(X, y, beta):
+    """
+    Mean squared error of linear regression model
+    :param X:   Input data matrix
+    :param y:   Target vector
+    :param beta: Estimated coefficient vector for the linear regression
+    :return:    Mean squared error
+    """
+
+    # add column of ones for the intercept
+    ones = np.ones((len(X), 1))
+    X = np.concatenate((ones, X), axis=1)
+    
+    # compute the error
+    epsilon = np.transpose(X.dot(beta) - y).dot(X.dot(beta) - y) / len(y)
+    
+    return epsilon
