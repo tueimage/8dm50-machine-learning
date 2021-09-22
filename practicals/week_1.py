3e6faea61089cf542f10e9a8504439d01458da28
Mariana Nunes <mariana.ferreira.nunes@tecnico.ulisboa.pt>
2021-09-22 18:14:20 +0200

    change of location of normal_pdf function (from /code to week_1.py)

--- practicals/week_1.py
+++ practicals/week_1.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import *
 
 def kNearestNeighbour(k, testData, trainingData, trainingLabels):
     """k Nearest neighbour algorithm for classification of data.
@@ -18,3 +19,10 @@ def kNearestNeighbour(k, testData, trainingData, trainingLabels):
     newLabels = np.array(newLabels)
         
     return newLabels
+
+def normal_pdf(x, mean, standard_deviation): #calculates the probability density function for a normal distribution
+    denominator = standard_deviation*sqrt(2*pi)
+    exponent = -((x-mean)**2)/(2*standard_deviation**2)
+    result = np.exp(exponent)/denominator #numpy.exp to calculate the exponential of all elements in the input array
+    
+    return result
